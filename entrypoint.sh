785106cae9f796c5c57c99df888152d63521e6a9
Reuben Lifshay <rlifshay@gmail.com>
2025-02-24 20:25:40 -0800

    feat: add MAILNAME option

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,22 @@ set -e
 test -n "$REPORT_EMAIL" && \
 	sed -Ei '/DEVICESCAN/ s/\-m root(@localhost)?( |$)/-m '"$REPORT_EMAIL"'\2/' /etc/smartd.conf
 
-if [ -n "$SMARTHOST" ] && [ ! -f $HOME/.mailrc ]; then
-	noscheme=$([ "${SMARTHOST#*://}" = "${SMARTHOST}" ] && echo 1) || :
-	noauth=$([ "${SMARTHOST#*@}" = "${SMARTHOST}" ] && echo 1) || :
-	cat <<-ENDCONFIG > $HOME/.mailrc
-		set v15-compat
-		${noauth:+set smtp-auth=none}
-		set mta=${noscheme:+smtp://}${SMARTHOST}
-	ENDCONFIG
+if [ -n "${MAILNAME}${SMARTHOST}" ] && [ ! -f $HOME/.mailrc ]; then
+	if [ -n "${SMARTHOST}" ]; then
+		noscheme=$([ "${SMARTHOST#*://}" = "${SMARTHOST}" ] && echo 1) || :
+		noauth=$([ "${SMARTHOST#*@}" = "${SMARTHOST}" ] && echo 1) || :
+	fi
+
+	{
+		echo "set v15-compat"
+		[ -n "$MAILNAME" ] &&
+			echo "set hostname=${MAILNAME}"
+		[ -n "$SMARTHOST" ] &&
+			cat <<-ENDCONFIG
+				${noauth:+set smtp-auth=none}
+				set mta=${noscheme:+smtp://}${SMARTHOST}
+			ENDCONFIG
+	} > $HOME/.mailrc
 fi
 
 [ "$1" = "smartd" ] && shift
